9d24ee9d5a8496b0e94bf372eb753358786374e1
Ironside999 <71923458+Ironside999@users.noreply.github.com>
2020-11-16 17:23:57 +0330

    Add files via upload

---
+++ me1.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+function search(){
+	document.getElementById("sizing5").style.width="170px";
+}
+
+function pull(){
+	document.getElementById("sizing5").style.width="110px";
+}
